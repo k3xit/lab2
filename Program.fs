5013d9f140bfa227f6613294748a90720a062027
kex1t <56569962+k3xit@users.noreply.github.com>
2026-03-05 22:06:02 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+
+let rec CreateList InputList =
+    printfn "Введите целое число для добавления в список: "
+    let inputLine = Console.ReadLine()
+    if (inputLine <> "") then 
+        match System.Int32.TryParse(inputLine) with
+        | (true, conInt) -> CreateList (InputList @ [conInt])
+        | _ -> 
+            printfn "Ошибка, повторите ввод"
+            CreateList InputList
+    else InputList
+let AbsoluteInt X =
+    if (X<0) then 
+        -X
+    else 
+        X
+
+let rec FirstNum X =
+    let Y = AbsoluteInt X
+    if ((Y/10)=0) then 
+        Y
+    else 
+        FirstNum (Y/10)
+[<EntryPoint>]
+let main args =
+    printfn "Создание изначального списка"
+    printfn "Введите пустую строку для конца"
+    let aList = CreateList [] 
+    let bList = List.map FirstNum aList
+
+    printfn "Итоговый список из первых цифр: %A" bList
+    0
